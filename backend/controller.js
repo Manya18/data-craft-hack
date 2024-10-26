06ecf72ed8210e4d5e4be114b81f1556fce5cdac
dariadariadariadariadaria <149558714+dariadariadariadariadaria@users.noreply.github.com>
2024-10-26 13:04:39 +0400

    error

--- backend/controller.js
+++ backend/controller.js
@@ -7,21 +7,35 @@ const upload = multer({ dest: 'uploads/' });
 
 const processFile = async (req, res, pool) => {
     const results = [];
+    let tableName = '';
+    let columns = [];
 
     try {
         fs.createReadStream(req.file.path)
             .pipe(csv())
-            .on('data', (data) => results.push(data))
+            .on('data', (data) => {
+                if (results.length === 0) {
+                    // Первая строка - название таблицы
+                    tableName = data[Object.keys(data)[0]]; // Предполагаем, что название таблицы в первом столбце
+                } else if (results.length === 1) {
+                    // Вторая строка - названия столбцов
+                    columns = Object.keys(data);
+                } else {
+                    // Остальные строки - данные
+                    results.push(data);
+                }
+            })
             .on('end', async () => {
                 try {
+                    console.log(tableName);
+                    console.log(results);
                     // Создание таблицы на основе заголовков
-                    const keys = Object.keys(results[0]);
-                    const keysWithTypes = keys.map(key => `${key} VARCHAR(255)`); // Замените VARCHAR(255) на нужный тип
-                    await pool.query(`CREATE TABLE IF NOT EXISTS your_table_2 (${keysWithTypes.join(', ')})`);
+                    const keysWithTypes = columns.map(key => `${key} TEXT`); // Замените TEXT на нужный тип
+                    await pool.query(`CREATE TABLE IF NOT EXISTS ${tableName} (${keysWithTypes.join(', ')})`);
 
                     // Вставка данных в таблицу
                     for (const row of results) {
-                        await pool.query(`INSERT INTO your_table_2(${keys.join(', ')}) VALUES(${keys.map(key => `'${row[key]}'`).join(', ')})`);
+                        await pool.query(`INSERT INTO ${tableName} (${columns.join(', ')}) VALUES(${columns.map(key => `'${row[key]}'`).join(', ')})`);
                     }
                     res.send('Data inserted into database');
                 } catch (err) {
