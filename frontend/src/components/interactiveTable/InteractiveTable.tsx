a3c165ad1bbcce574b2411c19d4add64fb092241
digischh <gavrilovadiana28@gmail.com>
2024-10-27 11:56:14 +0400

    type column

--- frontend/src/components/interactiveTable/InteractiveTable.tsx
+++ frontend/src/components/interactiveTable/InteractiveTable.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import styles from "./interactiveTable.module.css";
 import FiltersModal from "../modals/filtersModal/FiltersModal";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import SortModal from "../modals/filtersModal/SortModal";
 
 interface EditableCell {
   rowIndex: number | null;
@@ -19,16 +22,18 @@ const InteractiveTable = () => {
   });
   const [newValue, setNewValue] = useState("");
   const [changes, setChanges] = useState({});
+  const [filters, setFilters] = useState({ column: '', value: '' });
 
   const [isAddColumnModal, setIsAddColumnModal] = useState(false);
   const [isHideColumnModal, setIsHideColumnModal] = useState(false);
   const [isFilterModal, setIsFilterModal] = useState(false);
   const [isSortModal, setIsSortModal] = useState(false);
   const [isAddRowModal, setIsAddRowModal] = useState<boolean>(false);
+  const [isHideColumnsModalOpen, setIsHideColumnsModalOpen] = useState<boolean>(false);
+  const [hiddenColumns, setHiddenColumns] = useState<string[]>([]);
+  const [dataType, setDataType] = useState("");
 
-  const [filters, setFilters] = useState({column: '', value: ''});
-
-function getParameterFromUrl() {
+  function getParameterFromUrl() {
     const url = window.location.href;
     const urlParts = url.split('/');
     const parameter = urlParts[urlParts.length - 1];
@@ -41,10 +46,8 @@ function getParameterFromUrl() {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const filters_str = JSON.stringify(filters);
-        console.log(filters_str)
         const response = await fetch(
-            `http://localhost:8080/api/table?table=${tableTitle}${filters.column ? ('&filters=['+ JSON.stringify(filters) + ']'): ''}&page=${currentPage}`
+          `http://localhost:8080/api/table?table=${tableTitle}&page=${currentPage}`
         );
         const data = await response.json();
         setRows(data.data);
@@ -57,9 +60,8 @@ function getParameterFromUrl() {
       }
     };
     fetchData();
-  }, [currentPage, filters]);
-  
-//   /api/table?table=_11samara_tasks__1&sortBy=Пространство&sortOrder=desc&filters=[{"column":"Приоритет","value":"Средний"},{"column":"Пространство","value":"Москва"}]&page=1&limit=20
+  }, [currentPage]);
+
   const handleDoubleClick = (rowIndex: number, col: string, value: string) => {
     setEditableCell({ rowIndex, col });
     setNewValue(value);
@@ -79,7 +81,7 @@ function getParameterFromUrl() {
       setRows(updatedRows);
     }
 
-    const uniqueKey = `${(rows[rowIndex!] as any).id-1}-${col}`;
+    const uniqueKey = `${(rows[rowIndex!] as any).id - 1}-${col}`;
 
     setChanges((prev) => ({
       ...prev,
@@ -94,35 +96,73 @@ function getParameterFromUrl() {
     setNewValue("");
   };
 
-  const saveChanges = async() => {
+  const saveChanges = async () => {
     console.log(changes)
     try {
-        await fetch("http://localhost:8080/api/cell", {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({changes: changes, table_name: tableTitle}),
-        });
-        setChanges([]);
-        // setTrigger(tableTitle);
-      } catch (e) {
-        console.error(e);
-      }
+      await fetch("http://localhost:8080/api/cell", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ changes: changes, table_name: tableTitle }),
+      });
+      setChanges([]);
+      // setTrigger(tableTitle);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
+  const handleCheckboxChange = (e: any, column: string) => {
+    if (e.target.checked) {
+      setHiddenColumns(hiddenColumns.filter(col => col !== column));
+    } else {
+      setHiddenColumns([...hiddenColumns, column]);
+    }
+  };
+  const closeHideColumnsModal = () => {
+    setIsHideColumnsModalOpen(false);
+  };
+  const openHideColumnsModal = () => {
+    setIsHideColumnsModalOpen(true);
+  };
+
+  const handleUpdateColumnType = async (col: string) => {
+    try {
+      const responce = await fetch("http://localhost:8080/api/type", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ table_name: tableTitle, column_name: col, new_column_type: dataType }),
+      });
+      if (!responce.ok) toast.error('Невозможно конвертировать в данный тип');
+      setDataType("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <div className={styles.interactiveTable}>
-        <button onClick={() => {setIsFilterModal(true)}}>Фильтрация</button>
-        <button onClick={() => setIsHideColumnModal(true)}>Скрыть столбцы</button>
-        <button onClick={ () => {setIsSortModal(true)}}>Сортировка</button>
-        <button onClick={() => {setIsAddRowModal(true)}}>Добавить строку</button>
+      <button onClick={() => { setIsFilterModal(true) }}>Фильтрация</button>
+      <button onClick={() => openHideColumnsModal()}>Скрыть столбцы</button>
+      <button onClick={() => { setIsSortModal(true) }}>Сортировка</button>
+      <button onClick={() => { setIsAddRowModal(true) }}>Добавить строку</button>
       <div className={styles.tableWrapper}>
         <table>
           <thead>
             <tr>
               {columns.map((col, index) => (
-                <td key={index}>{col}</td>
+                <td key={index}>
+                  <div>  {col}</div>
+                  <select value={dataType} onChange={(e) => setDataType(e.target.value)}>
+                    <option value="">{dataType}</option>
+                    <option value="numeric">Числовой</option>
+                    <option value="text">Текст</option>
+                    <option value="date">Дата</option>
+                  </select>
+                  <button onClick={() => handleUpdateColumnType(col)}>Изменить тип</button>
+                </td>
               ))}
             </tr>
           </thead>
@@ -137,7 +177,7 @@ function getParameterFromUrl() {
                     }
                   >
                     {editableCell.rowIndex === (row as any).id &&
-                    editableCell.col === col ? (
+                      editableCell.col === col ? (
                       <input
                         type="text"
                         value={newValue}
@@ -182,7 +222,7 @@ function getParameterFromUrl() {
       </div>
       <div className={styles.actionButtons}>
         <button className={styles.button}>Отмена</button>
-        <button className={styles.button} onClick={() => {saveChanges()}}>Сохранить</button>
+        <button className={styles.button} onClick={() => { saveChanges() }}>Сохранить</button>
       </div>
       {/* Модальное окно для скрытия столбцов */}
       {/* {isHideColumnModal && (
@@ -206,13 +246,17 @@ function getParameterFromUrl() {
                 </div>
             )} */}
 
-            {/* Модальное окно для фильтрации */}
-            {isFilterModal && (
-                <FiltersModal columns={columns} table={tableTitle} setIsFilterModal={setIsFilterModal} setFilters={setFilters}></FiltersModal>
-            )}
+      {/* Модальное окно для фильтрации */}
+      {isFilterModal && (
+        <FiltersModal columns={columns} table={tableTitle} setIsFilterModal={setIsFilterModal} setFilters={setFilters}></FiltersModal>
+      )}
+      {/* 
+{isSortModal && (
+        <SortModal columns={columns} table={tableTitle} setIsSortModal={setIsSortModal} setModal={setModals}></SortModal>
+      )} */}
 
-            {/* Модальное окно сортировки */}
-            {/* {isSortModal && (
+      {/* Модальное окно сортировки */}
+      {/* {isSortModal && (
                 <div className={styles.modalOverlay}>
                     <div className={styles.modal}>
                         <h2>Сортировка</h2>
@@ -237,8 +281,8 @@ function getParameterFromUrl() {
                 </div>
             )} */}
 
-            {/* Модальное окно для добавления строки */}
-            {/* {isAddRowModal && (
+      {/* Модальное окно для добавления строки */}
+      {/* {isAddRowModal && (
                 <div className={styles.modalOverlay}>
                     <div className={styles.modal}>
                         <h2>Добавить новую строку</h2>
@@ -259,6 +303,8 @@ function getParameterFromUrl() {
                     </div>
                 </div>
             )} */}
+      <ToastContainer />
+
     </div>
   );
 };
